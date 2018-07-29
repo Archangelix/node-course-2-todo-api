9295c01d4e9570b1093f40b530ff32eebf730857
Iwan Satria <angelo_vargassi@yahoo.com>
2018-07-29 19:11:53 +0800

    Adding patch

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -59,6 +60,30 @@ app.delete('/todos/:id', (req, res) => {
       res.status(404).send({});
     }
   }).catch((e) => res.status(404).send({}));
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.send(404);
+  }
+
+  var body = _.pick(req.body, ['text', 'completed'])
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
